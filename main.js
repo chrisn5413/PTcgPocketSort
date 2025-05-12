2657554743e2b35e8db27a7ec96da653956e3fe0
Xeras <chrisn5413@gmail.com>
2025-05-12 01:48:08 -0700

    used await to get card data. wrote function to add info to the cards. Hopefully cards also save their divs for easy reinsertion

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 ﻿
-var response = fetch('https://raw.githubusercontent.com/chase-manning/pokemon-tcg-pocket-cards/refs/heads/main/v4.json')
-    /*.then(res => res.json())
+/*var response = fetch('https://raw.githubusercontent.com/chase-manning/pokemon-tcg-pocket-cards/refs/heads/main/v4.json')
+    .then(res => res.json())
     .then(data => {
         const container = document.getElementById('card-container');
         print(container)
@@ -12,3 +12,29 @@ var response = fetch('https://raw.githubusercontent.com/chase-manning/pokemon-tc
             container.appendChild(card);
         });
     });*/
+
+const url = 'https://raw.githubusercontent.com/chase-manning/pokemon-tcg-pocket-cards/refs/heads/main/v4.json'
+
+const POKEMON_PROMISE_RESULT = await fetch(url)
+const POKEMON_DATA = await POKEMON_PROMISE_RESULT.json()
+const CONTAINER = document.getElementById('card-container')
+
+
+// adds more details to card data
+// chronological id, html element + image, etc
+// todo: card description
+function updatePokemonData() {
+    let realId = 0
+    for (let pokemon of POKEMON_DATA) {
+        realId++
+        pokemon.realId = realId
+
+        const card = document.createElement('div')
+        card.className = 'card'
+        card.innerHTML = `<img src=\"${pokemon.image}\" alt=\"${pokemon.name} ${pokemon.realID}\">`
+        pokemon.div = card
+    }
+}
+function reloadCardContainer(newData) {
+    CONTAINER.replaceChildren(newData)
+}
