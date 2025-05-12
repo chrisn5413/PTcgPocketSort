e8bca18a89dea91276f48d331ef38a1e9bef6013
Xeras <chrisn5413@gmail.com>
2025-05-12 00:10:53 -0700

    wanted to test await in javascript so i'm commenting this out

--- main.js
+++ main.js
@@ -11,5 +11,4 @@ var response = fetch('https://raw.githubusercontent.com/chase-manning/pokemon-tc
             card.innerHTML = `<h3>${item.title}</h3><p>${item.description}</p>`;
             container.appendChild(card);
         });
-        
     });*/
