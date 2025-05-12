ffb42c5f0337654fe032159f654d4256869211ab
Xeras <chrisn5413@gmail.com>
2025-05-11 22:03:38 -0700

    Added core files. The javascript probably doesn't work and css is empty

---
+++ main.js
@@ -0,0 +1,15 @@
+﻿
+fetch('https://raw.githubusercontent.com/chase-manning/pokemon-tcg-pocket-cards/refs/heads/main/v4.json')
+    .then(res => res.json())
+    .then(data => {
+        const container = document.getElementById('card-container');
+        print(container)
+        /*
+        data.forEach(item => {
+            const card = document.createElement('div');
+            card.className = 'card';
+            card.innerHTML = `<h3>${item.title}</h3><p>${item.description}</p>`;
+            container.appendChild(card);
+        });
+        */
+    });
