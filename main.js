b5d7de0506e883d88a31ff446648fa8ef43b0a37
Xeras <chrisn5413@gmail.com>
2025-05-13 02:18:09 -0700

    shrinking, expand, and reset done. Switched back to divs containing the image elements so I can save the card descriptions under the div. sorting will be next, then searching text (need to get a better scraper to scrape card text)

--- main.js
+++ main.js
@@ -1,23 +1,7 @@
 ﻿
-/*var response = fetch('https://raw.githubusercontent.com/chase-manning/pokemon-tcg-pocket-cards/refs/heads/main/v4.json')
-    .then(res => res.json())
-    .then(data => {
-        const container = document.getElementById('card-container');
-        print(container)
-        
-        data.forEach(item => {
-            const card = document.createElement('div');
-            card.className = 'card';
-            card.innerHTML = `<h3>${item.title}</h3><p>${item.description}</p>`;
-            container.appendChild(card);
-        });
-    });*/
 
+// load pokemon from json url into a POKEMON_DATA, updating data and reloading page
 const url = 'https://raw.githubusercontent.com/chase-manning/pokemon-tcg-pocket-cards/refs/heads/main/v4.json';
-
-// const POKEMON_PROMISE_RESULT = await fetch(url)
-// const POKEMON_DATA = await POKEMON_PROMISE_RESULT.json()
-
 const CONTAINER = document.getElementById('card-container');
 let POKEMON_DATA;
 (async () => {
@@ -29,7 +13,7 @@ let POKEMON_DATA;
 
 
 // adds more details to card data
-// chronological id, html element + image, etc
+// chronological id, html image, etc
 // todo: card description
 function updatePokemonData() {
     let realId = 0;
@@ -40,26 +24,24 @@ function updatePokemonData() {
         let cardType = pokemon.health === "" ? "non-pokemon" : "pokemon";
         let fullart = pokemon.fullart === "Yes" ? " fullart" : '';
         let pack = ` pack:${pokemon.pack}`;
-
-        // const card = document.createElement('div')
-        // card.className = `${cardType} ${pokemon.realId} ${pokemon.name} ${pokemon.rarity}${fullart}${pack}`
-        // card.innerHTML = `<img src=\"${pokemon.image}\" alt=\"${pokemon.name} ${pokemon.realId}\">`
-        //
-        // pokemon.div = card
         
-        const card = document.createElement('img');
-        card.src = pokemon.image;
-        card.alt = `${pokemon.name} ${pokemon.realId}`;
-        card.className = `${cardType} ${pokemon.realId} ${pokemon.name} ${pokemon.rarity}${fullart}${pack}`;
+        let pokeDiv = document.createElement('div');
+        pokeDiv.className = `${cardType} ${pokemon.realId} ${pokemon.name} ${pokemon.rarity}${fullart}${pack}`;
         
-        pokemon.imgElement = card;
+        let pokeImg = document.createElement('img');
+        pokeImg.id = pokemon.realId;
+        pokeImg.src = pokemon.image;
+        pokeImg.alt = `${pokemon.name} ${pokemon.realId}`;
+        
+        pokeDiv.appendChild(pokeImg);
+        pokemon.pokeDiv = pokeDiv;
     }
 }
 
 // loads cards onto page given pokemon objects in an object
 function reloadCardContainer(newData) {
     if(typeof newData !== 'object')
-        return
+        return;
 
     // 30ms between every card loading
     let secondsBetweenLoading = 0.03;
@@ -79,27 +61,25 @@ function reloadCardContainer(newData) {
         }
         
         setTimeout(() => {
-            CONTAINER.append(pokemon.imgElement)
-        }, currentSeconds * 1000)
+            CONTAINER.append(pokemon.pokeDiv)
+        }, currentSeconds * 1000);
     }
 }
 
-function resetImageSize() {
-    //     // 1. Create the image element
-    // const img = document.createElement('img');
-
-    // // 2. Set the src attribute (required)
-    // img.src = 'path/to/your/image.jpg';
-
-    // // 3. Set the alt attribute (recommended for accessibility)
-    // img.alt = 'Description of the image';
-
-    // // 4. Optionally set other attributes like width, height, etc.
-    // img.width = 300;
-    // img.height = 200;
-
-    // // 5. Append the image element to the desired location in the DOM
-    // document.body.appendChild(img); // Example: append to the body
-    // // or
-    // // document.getElementById('some-element').appendChild(img);
+function expandAllImageSizes() {
+    const images = document.querySelectorAll('div img');
+    images.forEach(image => {
+        image.width += 75;
+    });
+}
+function shrinkAllImageSizes() {
+    const images = document.querySelectorAll('div img');
+    images.forEach(image => {
+        image.width -= 75;
+    });
+}
+function resetImages() {
+    for (let pokemon of POKEMON_DATA) {
+        pokemon.pokeDiv.querySelector('img').removeAttribute('width');
+    }
 }
